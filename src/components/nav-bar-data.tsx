121db9dfad31e222a4aa7c579115264fde3183c9
Krishna Kumar <dev@krishnaaa.com>
2023-12-23 19:04:10 +0530

    Navbar Update

---
+++ src/components/nav-bar-data.tsx
@@ -0,0 +1,150 @@
+"use client";
+
+import { useState } from "react";
+import { convertToInitials } from "@/lib/utils";
+import { Fragment } from "react";
+import { FaUser } from "react-icons/fa6";
+import LogoutButton from "./logout-button";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarMenuToggle,
+  NavbarItem,
+  Link,
+  Button,
+  NavbarMenuItem,
+  NavbarMenu,
+} from "@nextui-org/react";
+import { FiBook } from "react-icons/fi";
+import { FaChartColumn } from "react-icons/fa6";
+import { FaRegQuestionCircle } from "react-icons/fa";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+
+interface NavBarDataProps {
+  isAuthenticated: boolean;
+  name?: string | null;
+}
+
+const NavBarData = ({ isAuthenticated, name }: NavBarDataProps) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const path = usePathname();
+
+  const menuItems = [
+    {
+      name: "Rules",
+      link: "/rules",
+    },
+    {
+      name: "Leaderboard",
+      link: "/leaderboard",
+    },
+    {
+      name: "About",
+      link: "/about",
+    },
+  ];
+
+  return (
+    <Navbar onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+        <NavbarBrand>
+          <Image
+            src="/mlsa-logo.png"
+            width={40}
+            height={40}
+            alt="MLSA Logo"
+            priority={true}
+            quality={100}
+          />
+          <p className="ml-2 font-bold text-inherit">Kryptic Hunt</p>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden gap-4 sm:flex" justify="center">
+        <NavbarItem isActive={path.split("/")[1] === "rules"}>
+          <Link color="foreground" href="/rules">
+            <span className="flex items-center justify-center gap-2 hover:text-gray-100 hover:underline">
+              <FiBook />
+              Rules
+            </span>
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive={path.split("/")[1] === "leaderboard"}>
+          <Link href="/leaderboard">
+            <span className="flex items-center justify-center gap-2 hover:text-gray-100 hover:underline">
+              <FaChartColumn />
+              Leaderboard
+            </span>
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive={path.split("/")[1] === "about"}>
+          <Link color="foreground" href="/about">
+            <span className="flex items-center justify-center gap-2 hover:text-gray-100 hover:underline">
+              <FaRegQuestionCircle />
+              About
+            </span>
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarContent justify="end">
+        {isAuthenticated && (
+          <Fragment>
+            <NavbarItem>
+              <span className="flex items-center justify-between gap-2">
+                <FaUser />
+                <span className="hidden md:inline">{name ?? "User"}</span>
+                <span className="inline md:hidden">
+                  {convertToInitials(name ?? "U s e r")}
+                </span>
+              </span>
+            </NavbarItem>
+            <NavbarItem>
+              <LogoutButton />
+            </NavbarItem>
+          </Fragment>
+        )}
+        {!isAuthenticated && (
+          <Fragment>
+            <NavbarItem className="hidden lg:flex">
+              <Link href="/auth/login">Login</Link>
+            </NavbarItem>
+            <NavbarItem>
+              <Button
+                as={Link}
+                color="primary"
+                href="/auth/register"
+                variant="flat"
+              >
+                Sign Up
+              </Button>
+            </NavbarItem>
+          </Fragment>
+        )}
+      </NavbarContent>
+
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item.name}-${index}`}>
+            <Link
+              color={"foreground"}
+              className="w-full hover:underline"
+              href={item.link}
+              size="lg"
+            >
+              {item.name}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
+};
+
+export default NavBarData;
