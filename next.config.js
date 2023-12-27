7127ee0fa739b53972a264578140dbb9f8aca1e2
Krishna Kumar <dev@krishnaaa.com>
2023-12-28 01:52:06 +0530

    Removed /guide page and react-pdf

--- next.config.js
+++ next.config.js
@@ -5,6 +5,13 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  future: { webpack5: true },
+  webpack: (config) => {
+      config.resolve.alias.canvas = false
+      config.resolve.alias.encoding = false
+      return config
+  }
+}
 
 export default config;
--- src/app/guide/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Document, Page } from 'react-pdf';
-
-export default function Guide() {
-    return (
-        <Document file="public/guide.pdf">
-            <Page pageNumber={1} />
-        </Document>
-    );
-}
