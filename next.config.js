b0bb7bf60db9626a031b11129aa6e2cc06c3ea1b
Yashvardhan Singh <vs21yash@gmail.com>
2023-12-28 18:11:21 +0000

    new build

--- next.config.js
+++ next.config.js
@@ -1,29 +1,42 @@
 import "./src/env.js";
 
-const headers = () => [
-  {
-    key: "X-Content-Type-Options",
-    value: "nosniff",
-  },
-  {
-    key: "X-Frame-Options",
-    value: "SAMEORIGIN",
-  },
-  {
-    key: "X-XSS-Protection",
-    value: "1; mode=block",
-  },
-];
+const headers = async () => {
+  return [
+    {
+      source: "/(.*)",
+      headers: [
+        {
+          key: "X-Content-Type-Options",
+          value: "nosniff",
+        },
+        {
+          key: "X-Frame-Options",
+          value: "SAMEORIGIN",
+        },
+        {
+          key: "X-XSS-Protection",
+          value: "1; mode=block",
+        },
+      ],
+    },
+  ];
+};
 
-const redirects = async () => [
-  {
-    source: '/hakenkreuz',
-    destination: 'https://medium.com/msackiit/2022-and-thats-a-wrap-393def75a4f0',
-    permanent: true,
-  },
-];
+const redirects = async () => {
+  return [
+    {
+      source: '/hakenkreuz',
+      destination: 'https://medium.com/msackiit/2022-and-thats-a-wrap-393def75a4f0',
+      permanent: true,
+    },
+  ];
+};
 
 export default {
-  headers,
-  redirects,
+  async headers() {
+    return await headers();
+  },
+  async redirects() {
+    return await redirects();
+  },
 };
