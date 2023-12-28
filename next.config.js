ecf8073e0844e4d5d712e49f100994135053c8aa
Yashvardhan Singh <vs21yash@gmail.com>
2023-12-28 14:35:43 +0000

    merge branches

--- next.config.js
+++ next.config.js
@@ -3,15 +3,12 @@ import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
-    async redirects() {
-        return [
-            {
-                source: '/hakenkreuz',
-                destination: 'https://medium.com/msackiit/2022-and-thats-a-wrap-393def75a4f0',
-                permanent: true,
-            },
-        ]
-    },
+  future: { webpack5: true },
+  webpack: (config) => {
+      config.resolve.alias.canvas = false
+      config.resolve.alias.encoding = false
+      return config
+  }
 }
 
 export default config;
