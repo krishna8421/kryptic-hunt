6cf3fc1b79e460ebaca41a09c80bd357852581b6
Yashvardhan Singh <vs21yash@gmail.com>
2023-12-31 07:07:26 +0000

    final pr

--- src/app/end/page.tsx
+++ src/app/end/page.tsx
@@ -1,17 +1,19 @@
 const EndPage = () => {
   return (
     <div className="flex flex-col items-center justify-center text-xl mx-auto max-w-3xl">
+      <br />
       <div className="text-3xl font-bold">The hunt has concluded! Thank you for participating!</div>
       <div className="text-3xl font-bold"> We will be back with Bigger, Better and Kryptic 2.0</div>
-      <div className="text-left">
+      <br />     
+      <div className="text-left" style={{ fontSize: '17px' }}>
         👻 Prepare yourselves, tech enthusiasts! A chilling breeze is sweeping through the digital realm at KIIT-DU, heralding the arrival of an eerie fusion of innovation and passion in our upcoming event. Unravel the Code: Something supernatural awaits! Are you ready to transcend the boundaries of the ordinary?
       </div>
       <br />
-      <div className="text-center text-left">
+      <div className="text-left" style={{ fontSize: '17px' }}>
         🕸️ MLSA, KIIT Chapter proudly presents Project WingxRecruitment Drive 2024 – where the lines between reality and the unknown blur, and creativity takes a ghostly flight! Enlist in our event, and become part of the spectral revolution, where dreams morph into spectral entities and projects redefine the very essence of excellence. Dive into one of our haunted projects, spanning realms of technology to seize a ghostly ticket that opens the door to working closely with MLSA on various spine-chilling projects and possibly becoming a spectral core member of our community.
       </div>
       <br />
-      <div className="text-left">
+      <div className="text-left" style={{ fontSize: '17px' }}>
         👻 Haunt our socials https://linktr.ee/mlsakiit for the latest notifications on Project WingxRecruitment Drive 2024, decipher the spectral thrill, and join us in conjuring the future at the intersection of technology and ghostly creativity.
       </div>
       <br />
--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -29,7 +29,7 @@ const LeaderBoardPage = async () => {
         },
       },
     ],
-    take: 10,
+    take: 100,
   });
 
   const topPlayersUserIds = topPlayers.map((player) => player.userId);
